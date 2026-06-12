137a467378035e27d24c06ff01827ee7ca9a0887
Minecrftserver <foxking119112@gmail.com>
2026-06-12 21:05:27 +0300

    Minecrftserver

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
     "minecraft_version": "auto", // or specific version like "1.21.6"
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916, // set to -1 to automatically scan for open ports
+    "host": "VERO_MC.aternos.me:26762", // or "localhost", "your.ip.address.here"
+    "port": 26762, // set to -1 to automatically scan for open ports
     "auth": "offline", // or "microsoft"
 
     // the mindserver manages all agents and hosts the UI
-    "mindserver_port": 8080,
+    "mindserver_port": 26762,
     "auto_open_ui": true, // opens UI in browser on startup
     
     "base_profile": "assistant", // survival, assistant, creative, or god_mode
